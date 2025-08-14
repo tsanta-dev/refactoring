873153606d943887feafa711ce2195553297b02d
tsanta-dev <arisoatsanta@gmail.com>
2025-08-14 22:43:40 +0200

    Ajout de la méthode details() dans Humain

--- Glog/src/iut/bad/Humain.java
+++ Glog/src/iut/bad/Humain.java
@@ -4,4 +4,8 @@ public class Humain {
     protected String nom;
     protected String prenom;
     protected int age;
+    
+    public void details() {
+        System.out.println("Nom: " + nom + ", Prénom: " + prenom + ", Âge: " + age);
+    }
 }
